118b3e2542571df45b1bc359e0677cc3bdded9f7
otaviosumi <otaviosumi@hotmail.com>
2017-09-19 10:29:52 -0400

    minicurso

--- rac_1.py
+++ rac_1.py
@@ -1,7 +1,8 @@
 #https://education.github.com/pack
 
-from flask import Flask
+from flask import Flask, request
 import mongoengine as me
+import json
 
 class User(me.Document):
 	name = me.StringField()
@@ -14,16 +15,26 @@ class Task(me.Document):
 	title = me.StringField()
 	finished = me.BooleanField()
 	tags = me.ListField(me.StringField())
-	images = me.ListField(me.StringField())
+	user = me.ReferenceField(User)
+	color = me.StringField()
+
 
 
 
 
 app = Flask(__name__)
 
-@app.route("/sumi")
-def hello():
-	return "hello sumi-san"
+@app.route("/users", methods = ['GET'])
+def get_users():
+	users = User.objects.all()
+	array = []
+	for user in users:
+		array.append({
+			'id' : str(user._id),
+			'name' : user.name,
+			'email' : user.email
+			})
+	return 
 
 if __name__ == "__main__":
 	app.run()
