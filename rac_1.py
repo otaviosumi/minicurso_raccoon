df8d61c4204dcee7c4bb519f2f608e04821e2710
otaviosumi <otaviosumi@hotmail.com>
2017-09-19 10:47:18 -0400

    Senc - Aplicacoes WEB

--- rac_1.py
+++ rac_1.py
@@ -1,6 +1,6 @@
 #https://education.github.com/pack
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import mongoengine as me
 import json
 
@@ -24,17 +24,29 @@ class Task(me.Document):
 
 app = Flask(__name__)
 
+me.connect('todo_app')
+
 @app.route("/users", methods = ['GET'])
 def get_users():
 	users = User.objects.all()
 	array = []
 	for user in users:
 		array.append({
-			'id' : str(user._id),
+			'id' : str(user.id),
 			'name' : user.name,
 			'email' : user.email
 			})
+	return jsonify(array)
+
+"""
+@app.route("/users", methods = ['POST'])
+def post_users():
+	nome = #input de nome
+	iemail = #input de email
+	user = User(name = nome, email = iemail)
+	user.save()
 	return 
+"""
 
 if __name__ == "__main__":
 	app.run()
